384fee326b57d7f66c6a590901e51b313bc298b9
Yijiao Wang <ttwyj250@gmail.com>
2021-04-02 07:57:16 -0700

    Ingress-Nginx setup, create route handlers for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,16 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signOutRouter } from './routes/signout';
+import { signUpRouter } from './routes/signup';
 
 const app = express();
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi, there!');
-});
-
-app.post('/api/users/signup', (req, res) => {
-  const { email, password } = req.body;
-});
-
-app.post('/api/users/signin', (req, res) => {
-  const { email, password } = req.body;
-});
-
-app.post('/api/users/signout', (req, res) => {});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
