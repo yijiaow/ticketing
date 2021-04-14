83023ed6cb1c947958cb6c4bf886cb169888bd6a
Yijiao Wang <ttwyj250@gmail.com>
2021-04-13 21:53:31 -0700

    Add current-user route tests and auth cookie helper

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      generateCookie(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
@@ -27,3 +36,14 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.generateCookie = async () => {
+  const email = 'test@test.domain';
+  const password = 'password';
+  const resp = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+  const cookie = resp.get('Set-Cookie');
+  return cookie;
+};
