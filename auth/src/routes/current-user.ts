00e81a807a50d018b7b41bd63bc24cd5778b50ec
Yijiao Wang <ttwyj250@gmail.com>
2021-04-15 14:57:33 -0700

    Reusable apiClient to handle axios base urls and headers

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,13 +1,11 @@
 import express, { Request, Response } from 'express';
 import { currentUser } from '../middleware/currentUser';
-import { requireAuth } from '../middleware/requireAuth';
 
 const router = express.Router();
 
 router.get(
   '/api/users/current-user',
   currentUser,
-  requireAuth,
   (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
   }
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,35 +1,23 @@
-import axios from 'axios';
+import buildClient from '../utils/buildClient';
 
 const Home = ({ currentUser }) => {
   console.log('Current User', currentUser);
-  return (
+  return currentUser ? (
     <div>
-      <h1>Landing Page</h1>
+      <h1>You are signed in!</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>You are not signed in.</h1>
     </div>
   );
 };
 
-Home.getInitialProps = async ({ req, res }) => {
-  if (typeof window === 'undefined') {
-    // On the server
-    // Requests should be made to http://<service-name>.<namespace-name>.svc.cluster.local/...
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user',
-      {
-        // headers: {
-        //   Host: 'ticketing.dev',
-        // },
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // On the browser
-    // Requests can be made with base url of an empty string
-    const { data } = await axios.get('/api/users/current-user');
-    return data;
-  }
-  return {};
+Home.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/current-user');
+
+  return data;
 };
 
 export default Home;
