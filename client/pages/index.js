6b74e9c65db8d7ad8a3be3d4e928ec6413364b90
Yijiao Wang <ttwyj250@gmail.com>
2021-04-15 10:34:33 -0700

    Specify host, pass cookies in getInitialProps

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,35 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import axios from 'axios';
 
-export default function Home() {
+const Home = ({ currentUser }) => {
+  console.log('Current User', currentUser);
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <h1>Landing Page</h1>
     </div>
-  )
-}
+  );
+};
+
+Home.getInitialProps = async ({ req, res }) => {
+  if (typeof window === 'undefined') {
+    // On the server
+    // Requests should be made to http://<service-name>.<namespace-name>.svc.cluster.local/...
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/current-user',
+      {
+        // headers: {
+        //   Host: 'ticketing.dev',
+        // },
+        headers: req.headers,
+      }
+    );
+    return data;
+  } else {
+    // On the browser
+    // Requests can be made with base url of an empty string
+    const { data } = await axios.get('/api/users/current-user');
+    return data;
+  }
+  return {};
+};
+
+export default Home;
