08003ab4fb4108ef9225d21e9387af5779db7059
Yijiao Wang <ttwyj250@gmail.com>
2021-04-15 14:59:45 -0700

    Move page  to  component, add signin, signup pages

---
+++ client/components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const Header = ({ currentUser }) => {
+  console.log('currentUser in Header: ', currentUser);
+  const links = [
+    !currentUser && { label: 'Sign Up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => (
+      <li key={href} className="nav-item">
+        <Link href={href}>
+          <a>{label}</a>
+        </Link>
+      </li>
+    ));
+
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <Link href="/">
+        <a className="navbar-brand">TicketingDev</a>
+      </Link>
+
+      <div className="d-flex justify-content-end">
+        <ul className="nav d-flex align-items-center">{links}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/globals.css';
+import buildClient from '../utils/buildClient';
+import Header from '../components/Header';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps, currentUser }) => (
+  <>
+    <Header currentUser={currentUser} />
+    <Component {...pageProps} />
+  </>
+);
+
+MyApp.getInitialProps = async (appContext) => {
+  const apiClient = buildClient(appContext.ctx);
+  const { data } = await apiClient.get('/api/users/current-user');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+};
 
 export default MyApp;
