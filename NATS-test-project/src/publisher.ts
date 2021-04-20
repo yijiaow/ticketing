259b194a27a05f15026063512fe5ca831b87d2b4
Yijiao Wang <ttwyj250@gmail.com>
2021-04-19 21:58:25 -0700

    Await event publish

--- NATS-test-project/src/publisher.ts
+++ NATS-test-project/src/publisher.ts
@@ -8,13 +8,18 @@ const stan = nats.connect('nats-test-cluster', 'cluster_id_pub', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
-  new TicketCreatedPublisher(stan).publish({
-    id: '1',
-    title: 'Music Festival Ticket',
-    price: 199,
-    userId: randomBytes(4).toString('hex'),
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish({
+      id: '1',
+      title: 'Music Festival Ticket',
+      price: 199,
+      userId: randomBytes(4).toString('hex'),
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
