e6519bf4ef0326a45840ad7a9fcd04bde3f5abcc
Yijiao Wang <ttwyj250@gmail.com>
2021-04-22 20:58:47 -0700

    Test environment setup, build model for order ticket

--- orders/src/routes/create.ts
+++ orders/src/routes/create.ts
@@ -1,9 +1,35 @@
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundException,
+} from '@yijiao_ticketingdev/common';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Valid ticketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundException();
+    }
+
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
---
+++ orders/src/test/setup.ts
@@ -0,0 +1,61 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      generateCookie(): string[];
+    }
+  }
+}
+
+jest.mock('../natsWrapper');
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'somerandomkey';
+
+  mongo = new MongoMemoryServer();
+  const mongoUrl = await mongo.getUri();
+
+  await mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.generateCookie = () => {
+  // Create JWT with payload { id, email }
+  const token = jwt.sign(
+    {
+      id: new mongoose.Types.ObjectId().toHexString(),
+      email: 'test@test.domain',
+    },
+    process.env.JWT_KEY!
+  );
+
+  // Build session object
+  const session = { token };
+  const sessionJSON = JSON.stringify(session);
+
+  // Encode JSON string as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // Return a cookie with encoded data
+  return [`express:sess=${base64}`];
+};
