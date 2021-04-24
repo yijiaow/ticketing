a838a2228f0e98acd40c22c178855d846f960466
Yijiao Wang <ttwyj250@gmail.com>
2021-04-24 01:43:37 -0700

    Cancel order by orderId route

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,29 @@
 import express, { Request, Response } from 'express';
+import {
+  requireAuth,
+  NotFoundException,
+  NotAuthorizedException,
+} from '@yijiao_ticketingdev/common';
+import { Order, OrderStatus } from '../models/order';
 
 const router = express.Router();
 
-router.delete('/api/orders/:id', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.put(
+  '/api/orders/:id',
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id).populate('ticket');
+    if (!order) {
+      throw new NotFoundException();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedException();
+    }
+    order.set({ status: OrderStatus.Canceled });
+    await order.save();
+
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
