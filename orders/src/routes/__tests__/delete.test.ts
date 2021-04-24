e889b51c6238b6f8e913f40c78dd06a016be06f0
Yijiao Wang <ttwyj250@gmail.com>
2021-04-24 01:43:37 -0700

    Cancel order by orderId route

---
+++ orders/src/routes/__tests__/delete.test.ts
@@ -0,0 +1,32 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({ title: 'Coachella', price: 420 });
+  await ticket.save();
+  return ticket;
+};
+
+it('changes order status to `canceled`', async () => {
+  const ticket = await buildTicket();
+  const user = global.generateCookie();
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .put(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(200);
+
+  const updated = await Order.findById(order.id);
+  expect(updated!.status).toEqual(OrderStatus.Canceled);
+});
+
+it.todo('emits an order canceled event');
