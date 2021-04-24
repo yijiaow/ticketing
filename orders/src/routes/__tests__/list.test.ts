541a38484cd5ae71e6e148c8176cd96d5a50ed79
Yijiao Wang <ttwyj250@gmail.com>
2021-04-24 01:42:39 -0700

    Get a list of orders from current user route

---
+++ orders/src/routes/__tests__/list.test.ts
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({ title: 'Coachella', price: 420 });
+  await ticket.save();
+  return ticket;
+};
+
+it('get a list of orders for a specific user', async () => {
+  // Create three tickets
+  const ticket1 = await buildTicket();
+  const ticket2 = await buildTicket();
+  const ticket3 = await buildTicket();
+
+  const user1 = global.generateCookie();
+  const user2 = global.generateCookie();
+
+  // Create two orders as user #1
+  const { body: order1 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket1.id })
+    .expect(201);
+  const { body: order2 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user1)
+    .send({ ticketId: ticket2.id })
+    .expect(201);
+
+  // Create one order as user #2
+  const { body: order3 } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user2)
+    .send({ ticketId: ticket3.id })
+    .expect(201);
+
+  // Make request to get orders for user #1
+  const resp = await request(app)
+    .get('/api/orders')
+    .set('Cookie', user1)
+    .send()
+    .expect(200);
+  expect(resp.body.length).toEqual(2);
+  expect(resp.body[0].id).toEqual(order1.id);
+  expect(resp.body[1].id).toEqual(order2.id);
+  expect(resp.body[0].ticket.id).toEqual(ticket1.id);
+  expect(resp.body[1].ticket.id).toEqual(ticket2.id);
+});
