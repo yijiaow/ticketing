399e5b4c3eda32f8f47d95321f40c56b52c3bf8c
Yijiao Wang <ttwyj250@gmail.com>
2021-05-01 13:06:30 -0700

    Add listener for expiration complete event in orders service

---
+++ orders/src/events/listeners/expirationCompleteListener.ts
@@ -0,0 +1,30 @@
+import { Message } from 'node-nats-streaming';
+import {
+  BaseListener,
+  ExpirationCompleteEvent,
+  Subjects,
+  OrderStatus,
+} from '@yijiao_ticketingdev/common';
+import { queueGroupName } from './queueGroupName';
+import { Order } from '../../models/order';
+
+export class ExpirationCompleteListener extends BaseListener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationComplete;
+  queueGroupName = queueGroupName;
+
+  onMessage = async (
+    data: ExpirationCompleteEvent['data'],
+    message: Message
+  ) => {
+    const order = await Order.findById(data.orderId).populate('ticket');
+    if (!order) {
+      throw new Error('Order not found');
+    }
+    order.set({
+      status: OrderStatus.Canceled,
+    });
+    await order.save();
+
+    message.ack();
+  };
+}
