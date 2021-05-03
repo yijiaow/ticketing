051b1dbc7bf6b6c0c7489e6e40962917882edae6
Yijiao Wang <ttwyj250@gmail.com>
2021-05-02 23:58:49 -0700

    Payments service setup

---
+++ payments/src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import {
+  currentUser,
+  errorHandler,
+  NotFoundException,
+} from '@yijiao_ticketingdev/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundException();
+});
+
+app.use(errorHandler);
+
+export { app };
