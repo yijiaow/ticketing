40b529cacbbde9e0120066b93dbe2a3b91a7781e
Yijiao Wang <ttwyj250@gmail.com>
2021-05-05 20:03:28 -0700

    Filter out reserved tickets, add header links

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,13 +4,15 @@ const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
     !currentUser && { label: 'Sign In', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/create' },
+    currentUser && { label: 'My Orders', href: '/orders' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => (
       <li key={href} className="nav-item">
         <Link href={href}>
-          <a>{label}</a>
+          <a className="nav-link">{label}</a>
         </Link>
       </li>
     ));
